0b359e3d42c1e84c6c24e7bec58ba786c85543aa
Pablo López <pablo.lopez3895@gmail.com>
2020-02-23 12:35:26 -0300

    Style input

--- src/app/modules/module-number-speller/number-speller/number-speller.component.ts
+++ src/app/modules/module-number-speller/number-speller/number-speller.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-number-speller',
   templateUrl: './number-speller.component.html',
-  styleUrls: ['./number-speller.component.scss']
+  styleUrls: ['./number-speller.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class NumberSpellerComponent implements OnInit {
 
